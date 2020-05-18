ee9a3d6f4927f0e0f5b1dbc06046ae3518f7d011
Idan Pollak <idanpol1@gmail.com>
2020-05-18 18:47:35 +0300

    Updates

---
+++ precache-manifest.798714a89b14192a509dd6173e988da7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f298282b41f2056671b7298b365aa3",
+    "url": "/Covid19Stats/index.html"
+  },
+  {
+    "revision": "ad9835b06beda72ad7b7",
+    "url": "/Covid19Stats/static/js/2.87cf2a3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid19Stats/static/js/2.87cf2a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e381fc12efa1a826c636",
+    "url": "/Covid19Stats/static/js/main.5f478fce.chunk.js"
+  },
+  {
+    "revision": "19d388d6a461eb4b38bd",
+    "url": "/Covid19Stats/static/js/runtime-main.6fc54b4d.js"
+  }
+]);
